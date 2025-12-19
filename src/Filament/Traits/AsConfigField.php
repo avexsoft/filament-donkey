11ecfeb506c484471ab7aef7ecf84da6cf6c8cf9
mxts <53546313+mxts@users.noreply.github.com>
2025-12-19 10:37:42 +0800

    shift name to hint if label added

--- src/Filament/Traits/AsConfigField.php
+++ src/Filament/Traits/AsConfigField.php
@@ -2,13 +2,32 @@
 
 namespace Avexsoft\FilamentDonkey\Filament\Traits;
 
+use Closure;
 use Illuminate\Support\Str;
 
+/**
+ * Config key is shown in label by default
+ * If label is specified, then config key is shifted to hint
+ */
 trait AsConfigField
 {
     public static function make(?string $name = null): static
     {
-        return parent::make((string) Str::of($name)->replace('.', ':'))
+        $_this = parent::make((string) Str::of($name)->replace('.', ':'))
+            ->hint(false)
             ->label($name);
+
+        $_this->hint(null);
+
+        return $_this;
+    }
+
+    public function label(\Illuminate\Contracts\Support\Htmlable|Closure|string|null $label = null): static
+    {
+        if ($this->hint === null) {
+            $this->hint($this->name);
+        }
+
+        return parent::label($label);
     }
 }
