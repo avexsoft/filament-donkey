5463da81d862976ddf766aaf8355e69094400e15
Lightz2002 <68954690+lightz2002@users.noreply.github.com>
2025-12-28 11:40:28 +0800

    refactor project settings

---
+++ src/Filament/Pages/ConfigurationPage.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Avexsoft\FilamentDonkey\Filament\Pages;
+
+use Avexsoft\FilamentDonkey\Filament\Traits\AsOneConfigForm;
+use BackedEnum;
+use Filament\Actions\Action;
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Notifications\Notification;
+use Filament\Pages\Page;
+
+abstract class ConfigurationPage extends Page implements HasActions, HasForms
+{
+    use AsOneConfigForm;
+    use InteractsWithActions;
+    use InteractsWithForms;
+
+    protected static BackedEnum|string|null $navigationIcon = 'heroicon-o-document-text';
+
+    protected ?string $subheading = 'Various tests and settings on how the application behaves';
+
+    protected string $view = 'filament.pages.project-settings';
+
+    public ?array $data = [];
+
+    public static function form(\Filament\Schemas\Schema $schema): \Filament\Schemas\Schema
+    {
+        return $schema;
+    }
+
+    public function save(): void
+    {
+        $this->writeConfig($this->form->getState());
+
+        Notification::make()
+            ->success()
+            ->title('Success')
+            ->send();
+    }
+
+    protected function getFormActions(): array
+    {
+        return [
+            Action::make('save')
+                ->label('Save')
+                ->submit('save'),
+        ];
+    }
+}
--- src/Filament/Pages/ProjectSettings.php
+++ src/Filament/Pages/ProjectSettings.php
@@ -4,31 +4,10 @@ namespace Avexsoft\FilamentDonkey\Filament\Pages;
 
 use Avexsoft\FilamentDonkey\Filament\Forms\Components\ConfigTextInput;
 use Avexsoft\FilamentDonkey\Filament\Forms\Components\ConfigToggle;
-use Avexsoft\FilamentDonkey\Filament\Traits\AsOneConfigForm;
-use BackedEnum;
-use Filament\Actions\Action;
-use Filament\Actions\Concerns\InteractsWithActions;
-use Filament\Actions\Contracts\HasActions;
-use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Forms\Contracts\HasForms;
-use Filament\Notifications\Notification;
-use Filament\Pages\Page;
 use Filament\Schemas\Components\Section;
 
-class ProjectSettings extends Page implements HasActions, HasForms
+class ProjectSettings extends ConfigurationPage
 {
-    use AsOneConfigForm;
-    use InteractsWithActions;
-    use InteractsWithForms;
-
-    protected static BackedEnum|string|null $navigationIcon = 'heroicon-o-document-text';
-
-    protected ?string $subheading = 'Various tests and settings on how the application behaves';
-
-    protected string $view = 'filament.pages.project-settings';
-
-    public ?array $data = [];
-
     public static function form(\Filament\Schemas\Schema $schema): \Filament\Schemas\Schema
     {
         return $schema
@@ -44,24 +23,5 @@ class ProjectSettings extends Page implements HasActions, HasForms
                             ->label('Application debug mode'),
                     ]),
             ]);
-    }
-
-    public function save(): void
-    {
-        $this->writeConfig($this->form->getState());
-
-        Notification::make()
-            ->success()
-            ->title('Success')
-            ->send();
-    }
-
-    protected function getFormActions(): array
-    {
-        return [
-            Action::make('save')
-                ->label('Save')
-                ->submit('save'),
-        ];
     }
 }
